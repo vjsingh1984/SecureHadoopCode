c15b9aa76e89d53aa8882336d6b08ae4218da966
vjsingh1984 <V1jay001>
2016-03-29 16:21:56 -0500

    Tested KafkaPRoducer and KafkaConsumer with SASL

--- SecureKafkaDev/SecureConsumer/src/main/java/com/cloudera/sa/secureconsumer/SecureAsyncKafkaConsumer.java
+++ SecureKafkaDev/SecureConsumer/src/main/java/com/cloudera/sa/secureconsumer/SecureAsyncKafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.cloudera.sa.secureconsumer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Properties;
@@ -20,40 +22,44 @@ public class SecureAsyncKafkaConsumer {
 
   private final Properties props = new Properties();
 
-  public void setUp() {
+  public void setUp() throws UnknownHostException {
     props.put("bootstrap.servers", "SASL_PLAINTEXT://ip-10-20-0-5.us-west-2.compute.internal:9092,"
         + "SASL_PLAINTEXT://ip-10-20-0-5.us-west-2.compute.internal:9092");
     props.put("security.protocol", "SASL_PLAINTEXT");
-    props.put("group.id", "test");
+    props.put("client.id", this.getClass().getName() + "-" + InetAddress.getLocalHost().getHostName());
+    
+    props.put("group.id", "DemoTest");
     props.put("enable.auto.commit", "true");
     props.put("auto.commit.interval.ms", "1000");
     props.put("session.timeout.ms", "30000");
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     System.setProperty("java.security.auth.login.config", "/Users/vsingh/Software/KerberosLogin.conf");
   }
 
   public void demoConsumer() {
-    KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(props);
-    consumer.subscribe(Arrays.asList("foo", "bar"));
+    KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
+    consumer.subscribe(Arrays.asList("topic1"));
     long initialtimeInMillis = Calendar.getInstance().getTimeInMillis();
     long finaltimeInMillis = initialtimeInMillis + 1000;
     long currenttimeInMillis = Calendar.getInstance().getTimeInMillis();
-    while (finaltimeInMillis > currenttimeInMillis) {
-      ConsumerRecords<byte[], byte[]> records = consumer.poll(100);
-      for (ConsumerRecord<byte[], byte[]> record : records) {
-        System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+    while (true) {
+      ConsumerRecords<Integer, String> records = consumer.poll(100);
+      for (ConsumerRecord<Integer, String> record : records) {
+        System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
       }
 
       currenttimeInMillis = Calendar.getInstance().getTimeInMillis();
 
     }
+    
   }
 
   /**
    * @param args the command line arguments
+   * @throws java.net.UnknownHostException
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws UnknownHostException {
 
     SecureAsyncKafkaConsumer secureKafkaConsumerClient = new SecureAsyncKafkaConsumer();
     secureKafkaConsumerClient.setUp();
--- SecureKafkaDev/SecureProducer/src/main/java/com/cloudera/sa/secureproducer/SecureAsyncKafkaProducer.java
+++ SecureKafkaDev/SecureProducer/src/main/java/com/cloudera/sa/secureproducer/SecureAsyncKafkaProducer.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Properties;
-import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -24,39 +22,37 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 public class SecureAsyncKafkaProducer {
 
   Properties props = new Properties();
-  Properties propsKerberos ;
-  
+  Properties propsKerberos;
 
   public void setUp() throws UnknownHostException, IOException {
-    
+
     props.put("client.id", this.getClass().getName() + "-" + InetAddress.getLocalHost().getHostName());
     props.put("acks", "all");
     props.put("retries", 0);
     props.put("batch.size", 16384);
     props.put("linger.ms", 1);
     props.put("buffer.memory", 33554432);
-    props.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-    props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-    props.put("bootstrap.servers", "SASL_PLAINTEXT://ip-10-20-0-5.us-west-2.compute.internal:9092,"+
-        "SASL_PLAINTEXT://ip-10-20-0-5.us-west-2.compute.internal:9092");
-    props.put("security.protocol","SASL_PLAINTEXT");
-    
+    props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("bootstrap.servers", "SASL_PLAINTEXT://ip-10-20-0-5.us-west-2.compute.internal:9092,"
+        + "SASL_PLAINTEXT://ip-10-20-0-5.us-west-2.compute.internal:9092");
+    props.put("security.protocol", "SASL_PLAINTEXT");
+
     // Setup Kerberos Properites
     Properties propsKerberos = System.getProperties();
     propsKerberos.put("java.security.auth.login.config", "/Users/vsingh/Software/KerberosLogin.conf");
     System.setProperties(propsKerberos);
 
   }
-  
 
   public void demoProducer() {
-    
-    Producer<byte[], byte[]> producer = new KafkaProducer<>(props);
 
-    for (int i = 0; i < 10; i++) {
-      ProducerRecord<byte[], byte[]> record = new ProducerRecord<>("topic1", Bytes.toBytes(i), Bytes.toBytesBinary("Message-" + i));
+    Producer<Integer, String> producer = new KafkaProducer<>(props);
 
-      producer.send(record/*,
+    for (int i = 0; i < 10000; i++) {
+      ProducerRecord<Integer, String> record = new ProducerRecord<>("topic1", Integer.valueOf(i), "Message-" + i);
+
+      producer.send(record,
           new Callback() {
             @Override
             public void onCompletion(RecordMetadata meta, Exception e) {
@@ -66,8 +62,10 @@ public class SecureAsyncKafkaProducer {
                 System.out.println("Msg Successful Ack: [Offset,Partition]=[" + meta.offset() + "," + meta.partition() + "]");
               }
             }
-          }*/);
+          });
     }
+    producer.flush();
+    producer.close();
   }
 
   public static void main(String[] args) throws UnknownHostException, IOException {
